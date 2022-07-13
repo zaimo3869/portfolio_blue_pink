30812fe9fd63693377e2e34559e13cbad5e62989
zaimo3869 <zaimo3869@gmail.com>
2022-07-13 17:17:44 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import { BrowserRouter } from "react-router-dom";
+import './App.css';
+import Home from './component/home';
+
+
+function App() {
+  return (
+    
+       
+        <BrowserRouter>
+        <Home></Home>
+        
+        </BrowserRouter>
+        
+       
+  );
+}
+
+export default App;
